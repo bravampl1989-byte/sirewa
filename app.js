e0471f6d0249661ad97219ec8659a461247845f8
bravampl1989-byte <brava.mpl1989@gmail.com>
2026-07-10 16:34:02 +0700

    Update server listen to include host address

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { server } = require("./server");
 
 const PORT = process.env.PORT || 3000;
+const HOST = "127.0.0.1";
 
-server.listen(PORT, () => {
-  console.log(`SIREWA berjalan di port ${PORT}`);
+server.listen(PORT, HOST, () => {
+  console.log(`SIREWA berjalan di http://${HOST}:${PORT}`);
 });
